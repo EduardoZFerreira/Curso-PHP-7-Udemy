8b3343dbcc6f765ee5620f984ee9cbadc051d875
Eduardo Ferreira <eduardo.zekl@gmail.com>
2019-03-18 22:18:15 -0300

    Aulas sobre variáveis

---
+++ Variáveis/index.php
@@ -0,0 +1,26 @@
+<?php
+
+    $nome = "HCode";
+
+    echo $nome . "<br>";
+
+    echo "<br>";
+
+    var_dump($nome);
+    
+    echo "<br>";
+
+    $AnoNascimento = "1998";
+
+    echo "<br>";
+
+    $nome1 = "Eduardo";
+
+    echo $nome1;
+
+    unset($nome1);
+
+    echo "<br>";
+
+    if(isset($nome1)) echo $nome1; 
+    else echo "Var unset";
---
+++ Variáveis/tipos.php
@@ -0,0 +1,12 @@
+<?php
+
+    $nome = "HCode";
+    $site = "www.udemy.com";
+    $ano = 1998;
+    $salario = 5500.99;
+    $bloqueado = false;
+
+    $frutas = array('Abacaxi', 'Laranja', 'Manga');
+
+
+    $data = new DateTime();
