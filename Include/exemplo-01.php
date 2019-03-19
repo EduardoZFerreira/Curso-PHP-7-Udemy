7308f6461cc8b22d5844ad8185f4639466d50d3c
Eduardo Ferreira <eduardo.zekl@gmail.com>
2019-03-19 12:47:16 -0300

    Include e require

---
+++ Include/exemplo-01.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump("Ok");
+
+function Somar($a, $b)
+{
+    return $a + $b;
+}
---
+++ Include/exemplo-02.php
@@ -0,0 +1,10 @@
+<?php
+// include "exemplo-01.php";
+
+// require "exemplo-01.php";
+
+
+require_once "exemplo-01.php";
+require_once "exemplo-01.php";
+
+echo Somar(10, 20);
