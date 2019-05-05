b4e8f991499a2aa52b9615281dd8c567f211a3ac
Eduardo Ferreira <eduardo.zekl@gmail.com>
2019-05-05 12:32:56 -0300

    Excluindo arquivos com UNLINK

---
+++ UNLINK/exemplo-02.php
@@ -0,0 +1,13 @@
+<?php
+
+if(!is_dir("Images"))
+    mkdir("Images");
+
+foreach (scandir("Images") as $item) {
+    if(!in_array($item, array(".", "..")))
+    {
+        unlink("Images/" . $item);
+    }
+}
+
+echo "OK";
